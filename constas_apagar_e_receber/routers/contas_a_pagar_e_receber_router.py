1581faae19ca2fbe4724c9b569848335866f9cbe
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-22 09:52:18 -0300

    GET e POST

---
+++ constas_apagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -0,0 +1,75 @@
+from decimal import Decimal
+from typing import List
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+
+router = APIRouter(prefix="/constas_apagar_e_receber")
+
+class ContaPagarReceberResponse(BaseModel):
+    id: int
+    descricao: str
+    valor: Decimal
+    tipo: str # PAGAR, RECEBER
+
+class ContaPagarReceberRequest(BaseModel):
+    descricao: str
+    valor: Decimal
+    tipo: str # PAGAR, RECEBER
+
+@router.get("/", response_model=List[ContaPagarReceberResponse])
+def listar_contas():
+    return [
+        ContaPagarReceberResponse(
+            id=1,
+            descricao="Aluguel",
+            valor=450,
+            tipo="PAGAR"
+        ),
+        ContaPagarReceberResponse(
+            id=2,
+            descricao="Luz",
+            valor=250,
+            tipo="PAGAR"
+        ),
+        ContaPagarReceberResponse(
+            id=3,
+            descricao="Aguá",
+            valor=70,
+            tipo="PAGAR"
+        ),
+        ContaPagarReceberResponse(
+            id=4,
+            descricao="Mercado",
+            valor=800,
+            tipo="PAGAR"
+        ),
+        ContaPagarReceberResponse(
+            id=5,
+            descricao="Internet",
+            valor=89,
+            tipo="PAGAR"
+        ),
+        ContaPagarReceberResponse(
+            id=6,
+            descricao="Cartão de credito",
+            valor=500,
+            tipo="PAGAR"
+        ),
+        ContaPagarReceberResponse(
+            id=7,
+            descricao="Salário",
+            valor=2000,
+            tipo="RECEBER"
+        )
+    ]
+    
+
+@router.post("/", response_model=ContaPagarReceberResponse, status_code=201)
+def criar_conta(conta: ContaPagarReceberRequest):
+    return ContaPagarReceberResponse(
+        id=8,
+        descricao=conta.descricao,
+        valor=conta.valor,
+        tipo=conta.tipo
+    )
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 
+from constas_apagar_e_receber.routers.contas_a_pagar_e_receber_router import router
+
 
 app = FastAPI()
 
@@ -8,3 +10,5 @@ app = FastAPI()
 @app.get("/")
 def ola_sou_programador():
     return "Oi, eu sou programdor"
+
+app.include_router(router)
